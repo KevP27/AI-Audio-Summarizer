dc3515926d0528dcbdbc93a991da6d103526c0ac
Park, Kevin Kyungje <kpark373@gatech.edu>
2024-04-09 18:48:17 -0400

    Fixed db creation

--- main.py
+++ main.py
@@ -8,22 +8,10 @@ from wtforms.validators import InputRequired
 from werkzeug.utils import secure_filename
 import os
 from answerQuestion import answerQ
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aiaudiosummarizersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///files.db' 
-
-db = SQLAlchemy(app)
-
-class File(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(120), unique=True, nullable=False)
-    path = db.Column(db.String(120), unique=True, nullable=False)
-
-with app.app_context():
-    db.create_all()
 
 class uploadFile(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
@@ -40,9 +28,6 @@ def index():
                 filename = secure_filename(file.filename)
                 save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 file.save(save_path)
-                new_file = File(filename=filename, path=save_path)
-                db.session.add(new_file)
-                db.session.commit()
 
                 return jsonify({'filename': filename})
 
@@ -51,9 +36,9 @@ def index():
 @app.route('/summarize', methods=['POST'])
 def summarize():
     filename = request.form.get('filename')
-    file_record = File.query.filter_by(filename=filename).first()
-    if file_record:
-        summary = audio_summarizer(file_record.path) 
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if os.path.exists(file_path):
+        summary = audio_summarizer(file_path) 
         return jsonify({'summary': summary})
     else:
         return jsonify({'error': 'File not found'}), 404
@@ -69,4 +54,4 @@ def answer_question():
         return jsonify({'error': 'No question provided'}), 400
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
